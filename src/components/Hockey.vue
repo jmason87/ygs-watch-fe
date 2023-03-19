6653d073463556f45ce4a2e6ceac1172b1e38108
jmason87 <joewebdev87@gmail.com>
2023-03-19 13:27:35 -0500

    vgt table working with store data

--- src/components/Hockey.vue
+++ src/components/Hockey.vue
@@ -10,43 +10,51 @@
 import { mapGetters, mapActions } from 'vuex'
 
 export default {
-    data(){
-        return {
-        columns: [
-            {
-            label: 'Name',
-            field: 'name',
-            },
-            {
-            label: 'Age',
-            field: 'age',
-            type: 'number',
-            },
-            {
-            label: 'Created On',
-            field: 'createdAt',
-            type: 'date',
-            dateInputFormat: 'yyyy-MM-dd',
-            dateOutputFormat: 'MMM do yy',
-            },
-            {
-            label: 'Percent',
-            field: 'score',
-            type: 'percentage',
-            },
-        ],
-        rows: [
-            { id:1, name:"John", age: 20, createdAt: '',score: 0.03343 },
-            { id:2, name:"Jane", age: 24, createdAt: '2011-10-31', score: 0.03343 },
-            { id:3, name:"Susan", age: 16, createdAt: '2011-10-30', score: 0.03343 },
-            { id:4, name:"Chris", age: 55, createdAt: '2011-10-11', score: 0.03343 },
-            { id:5, name:"Dan", age: 40, createdAt: '2011-10-21', score: 0.03343 },
-            { id:6, name:"John", age: 20, createdAt: '2011-10-31', score: 0.03343 },
-        ],
-        };
-    },
     computed: {
-        ...mapGetters('hockey', ['allPlayers'])
+        ...mapGetters('hockey', ['allPlayers']),
+        rows() {
+            return this.allPlayers.map(player => ({
+                name: player.name,
+                position: player.position,
+                age: player.age,
+                birthdate: player.birthdate,
+                year_drafted: player.year_drafted,
+                round: player.round,
+                pick: player.pick
+            }))
+        },
+        columns() {
+            return [
+                {
+                label: 'Name',
+                field: 'name',
+                },
+                {
+                label: 'Position',
+                field: 'position',
+                },
+                {
+                label: 'Age',
+                field: 'age',
+                },
+                {
+                label: 'Birthdate',
+                field: 'birthdate',
+                },
+                {
+                label: 'Year Drafted',
+                field: 'year_drafted',
+                },
+                {
+                label: 'Round',
+                field: 'round',
+                },
+                {
+                label: 'Pick',
+                field: 'pick',
+                }
+            ]
+        }
     },
     created() {
         this.initializePlayers()
